f47718bb153baf59d811341b8ddd2699b385f0b1
Girish Vazheliparambil Girijan <738048@student.tor.loc>
2019-05-29 10:14:37 -0400

    A-A

--- C0738048/C0738048/Program.cs
+++ C0738048/C0738048/Program.cs
@@ -10,6 +10,19 @@ namespace C0738048
     {
         static void Main(string[] args)
         {
+            AA a = new AA();
+            a.Run(); // calling function in AA class
+            Console.ReadLine();
         }
     }
+    class AA
+    {
+        public void Run()
+        {
+            Console.WriteLine("Enter your name");  
+            string name = Console.ReadLine();// Reading name
+            Console.WriteLine("Hello! {0}", name); // Displaying name 
+        }
+    }
+    
 }
